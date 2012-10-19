9d3ed802d20c80468d6854a5b4f978d07d927ef8
Ari Bodaghee <abodaghee@fullsail.edu>
2012-10-18 20:32:58 -0400

    hello! i DID NOTHING

--- Source/Sapphire/Sapphire/Form1.Designer.cs
+++ Source/Sapphire/Sapphire/Form1.Designer.cs
@@ -51,13 +51,18 @@
             this.FramesPerSecond = new System.Windows.Forms.Label();
             this.FramesPerSecondLabel = new System.Windows.Forms.Label();
             this.graphicsPanel1 = new Sapphire.GraphicsPanel(this.components);
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
             this.SapphireMenu.SuspendLayout();
             this.SapphireControl.SuspendLayout();
             this.RenderTab.SuspendLayout();
             this.RenderSubControl.SuspendLayout();
             this.PostProcessTab.SuspendLayout();
             this.PostProcessEffects.SuspendLayout();
+            this.EffectTab.SuspendLayout();
             this.graphicsPanel2.SuspendLayout();
+            this.tabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // SapphireMenu
@@ -232,6 +237,7 @@
             // EffectTab
             // 
             this.EffectTab.BackColor = System.Drawing.Color.Black;
+            this.EffectTab.Controls.Add(this.tabControl1);
             this.EffectTab.Location = new System.Drawing.Point(4, 22);
             this.EffectTab.Name = "EffectTab";
             this.EffectTab.Padding = new System.Windows.Forms.Padding(3);
@@ -279,6 +285,36 @@
             this.graphicsPanel1.Size = new System.Drawing.Size(640, 480);
             this.graphicsPanel1.TabIndex = 0;
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(7, 7);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(323, 441);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(315, 415);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(192, 74);
+            this.tabPage3.TabIndex = 1;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
             // SapphireForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -301,8 +337,10 @@
             this.PostProcessTab.ResumeLayout(false);
             this.PostProcessEffects.ResumeLayout(false);
             this.PostProcessEffects.PerformLayout();
+            this.EffectTab.ResumeLayout(false);
             this.graphicsPanel2.ResumeLayout(false);
             this.graphicsPanel2.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -331,6 +369,9 @@
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.GroupBox PostProcessEffects;
         public System.Windows.Forms.ComboBox PostProcessComboBox;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage3;
 
     }
 }
